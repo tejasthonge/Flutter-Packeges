123c3fda17d6f2218cbd295f32abbcd982e2ea9a
tejasthonge <amarthonge6767@gmail.com>
2025-02-14 16:30:11 +0530

    not working

---
+++ 11_flutter_phone_direct_caller/calling/android/app/src/main/kotlin/com/example/calling/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.boss.calling
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ 11_flutter_phone_direct_caller/calling/lib/diler.dart
@@ -0,0 +1,18 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class Diler extends StatefulWidget {
+  const Diler({super.key});
+
+  @override
+  State<Diler> createState() => _DilerState();
+}
+
+class _DilerState extends State<Diler> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
